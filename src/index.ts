872e605a8a9021b5d0d6f17d8281c598c0ee452e
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2021-01-29 14:38:51 -0500

    decoradores 2

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,3 @@
     ===== Código de TypeScript =====
 */
 
-// los decoradores son funciones
-function classDecorator<T extends {new (...args: Array<any>): {}}>(constructor: T) {
-    return class extends constructor {
-        newProperty: 'new';
-        hello: 'override';
-    }
-}
-
-@classDecorator
-class miSuperClase {
-    public miPropiedad: string;
-
-    public imprimir() {
-        console.log('Hola');
-    }
-}
-
-console.log('decoradores', miSuperClase);
---
+++ src/tipos-basicos/decoradores.ts
@@ -0,0 +1,18 @@
+// los decoradores son funciones
+function classDecorator<T extends {new (...args: Array<any>): {}}>(constructor: T) {
+    return class extends constructor {
+        newProperty: 'new';
+        hello: 'override';
+    }
+}
+
+@classDecorator
+class miSuperClase {
+    public miPropiedad: string;
+
+    public imprimir() {
+        console.log('Hola');
+    }
+}
+
+console.log('decoradores', miSuperClase);
