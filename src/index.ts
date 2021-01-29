c8eafbb97eb32b7a5a9af5d0ac52ae56c90f6c14
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2021-01-29 11:10:30 -0500

    destructuring

--- src/index.ts
+++ src/index.ts
@@ -2,32 +2,27 @@
     ===== Código de TypeScript =====
 */
 
-
-interface Direccion {
-    calle: string;
-    pais: string;
-    ciudad: string;
+interface Reproductor {
+    volumen: number;
+    segundo: number;
+    cancion: string;
+    detalles: Detalles;
 }
 
-interface SuperHeroe {
-    nombre: string;
-    edad: number;
-    direccion: Direccion;
-    mostrarDireccion: () => string;
+interface Detalles {
+    autor: string;
+    anio: number;
 }
 
-const superHeroe: SuperHeroe = {
-    nombre: 'Spiderman',
-    edad: 30,
-    direccion: {
-        calle: 'Main St',
-        pais: 'Usa',
-        ciudad: 'NY'
-    },
-    mostrarDireccion() {
-        return `${this.nombre}, ${this.direccion.calle}, ${this.direccion.ciudad}`;
-    } 
-}
+const reproductor: Reproductor = {
+    cancion: 'xD',
+    volumen: 90,
+    segundo: 12,
+    detalles: {
+        autor: 'pepe',
+        anio: 2020
+    }
+};
 
-const direccion = superHeroe.mostrarDireccion();
-console.log(direccion);
+const {cancion, volumen, segundo, detalles: {autor, anio}} = reproductor;
+console.log(autor, anio, cancion);
---
+++ src/tipos-basicos/tipado-interfaces.ts
@@ -0,0 +1,30 @@
+
+
+interface Direccion {
+    calle: string;
+    pais: string;
+    ciudad: string;
+}
+
+interface SuperHeroe {
+    nombre: string;
+    edad: number;
+    direccion: Direccion;
+    mostrarDireccion: () => string;
+}
+
+const superHeroe: SuperHeroe = {
+    nombre: 'Spiderman',
+    edad: 30,
+    direccion: {
+        calle: 'Main St',
+        pais: 'Usa',
+        ciudad: 'NY'
+    },
+    mostrarDireccion() {
+        return `${this.nombre}, ${this.direccion.calle}, ${this.direccion.ciudad}`;
+    } 
+}
+
+const direccion = superHeroe.mostrarDireccion();
+console.log(direccion);
