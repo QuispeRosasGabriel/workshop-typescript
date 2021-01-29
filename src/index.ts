52cfb41e50668259d295344ab0b45f4e66d5662b
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2021-01-29 14:25:17 -0500

    tipos genericos

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,10 @@
     ===== Código de TypeScript =====
 */
 
+
+function queTipoSoy<T>(argumento: T) {
+    return argumento;
+}
+
+let soyString = queTipoSoy('Hola Mundo');
+let soyNumero = queTipoSoy(100);
