9863430a43c59db580eb590d54a6f93b557d391d
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2021-01-29 14:06:06 -0500

    clases

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 /*
     ===== Código de TypeScript =====
 */
+
+class Heroe {
+    private alterEgo: string;
+    public edad: number;
+    static nombreReal: string;
+
+    imprimirNombre(){
+        return this.alterEgo + ' ' + this.edad;
+    }
+
+}
+
+const spiderMan = new Heroe();
+console.log(spiderMan);
